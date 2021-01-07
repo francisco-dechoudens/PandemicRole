5ec304b363f0a811adf69cf856e6e4965646c0a0
Francisco <francisco.dechoudens@abarcahealth.com>
2021-01-07 12:43:24 -0400

    Add ViewModel

---
+++ PandemicRole/ViewModels/SelectedRolePageViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using PandemicRole.Infrastructure.Globalization;
+
+namespace PandemicRole.ViewModels
+{
+    public class SelectedRolePageViewModel : BaseViewModel
+    {
+        public SelectedRolePageViewModel()
+        {
+            this.role = Localization.Role_15;
+            this.roleDescription = Localization.Role_15_Description.Replace("\\n", Environment.NewLine);
+        }
+
+        private string role;
+        public string Role
+        {
+            get
+            {
+                return this.role;
+            }
+
+            set
+            {
+                if (this.role == value)
+                {
+                    return;
+                }
+
+                this.role = value;
+                this.NotifyPropertyChanged();
+            }
+        }
+
+        private string roleDescription;
+        public string RoleDescription
+        {
+            get
+            {
+                return this.roleDescription;
+            }
+
+            set
+            {
+                if (this.roleDescription == value)
+                {
+                    return;
+                }
+
+                this.roleDescription = value;
+                this.NotifyPropertyChanged();
+            }
+        }
+    }
+}
