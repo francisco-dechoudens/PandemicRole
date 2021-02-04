f61fd3c0fc1522f4e00b33017a4a382a4d032d1f
Francisco <francisco.dechoudens@abarcahealth.com>
2021-02-04 17:22:23 -0400

    Change app icon for android

--- PandemicRole.Android/Resources/Resource.designer.cs
+++ PandemicRole.Android/Resources/Resource.designer.cs
@@ -9328,6 +9328,9 @@ namespace PandemicRole.Droid
 			// aapt resource value: 0x7F0C0002
 			public const int launcher_foreground = 2131492866;
 			
+			// aapt resource value: 0x7F0C0003
+			public const int myIcon = 2131492867;
+			
 			static Mipmap()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
